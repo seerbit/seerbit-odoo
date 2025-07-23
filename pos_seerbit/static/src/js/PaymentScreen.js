f618a30a25190aaca51d3e28e26e8c6a3db015f7
kanebi <emmakanebi@gmail.com>
2025-07-23 22:33:58 +0100

    paymentine fix12

--- pos_seerbit/static/src/js/PaymentScreen.js
+++ pos_seerbit/static/src/js/PaymentScreen.js
@@ -2,42 +2,49 @@
 
 import { PaymentScreen } from '@point_of_sale/app/screens/payment_screen/payment_screen';
 import { patch } from '@web/core/utils/patch';
+import { onMounted } from '@odoo/owl';
 
 patch(PaymentScreen.prototype, {
-    setup(superSetup) {
-        superSetup();
-        this.send_force_done = this.send_force_done.bind(this);
-        this.send_payment_request = this.send_payment_request.bind(this);
-    },
-    send_force_done(line) {
-        if (line && line.payment_method && line.payment_method.payment_terminal) {
-            const iface = this.pos.payment_interfaces[line.payment_method.payment_terminal];
-            if (iface && iface.send_force_done) {
-                iface.send_force_done(line);
+    setup() {
+        super.setup();
+        onMounted(() => {
+            const pendingPaymentLine = this.currentOrder.paymentlines.find(
+                paymentLine => paymentLine.payment_method.use_payment_terminal === 'seerbit' &&
+                    (!paymentLine.is_done() && paymentLine.get_payment_status() !== 'pending')
+            );
+            if (pendingPaymentLine) {
+                const paymentTerminal = pendingPaymentLine.payment_method.payment_terminal;
+                pendingPaymentLine.set_payment_status('waitingSeerbit');
             }
+        });
+    },
+
+    // We are adding the new methods here
+    
+    /**
+     * This method is called when the 'Retry' button is clicked.
+     * It finds the correct payment interface and calls its send_payment_request method.
+     * @param {Object} line The payment line object passed from the template.
+     */
+    async send_payment_request(line) {
+        const payment_line = this.currentOrder.get_paymentline(line.cid);
+        if (payment_line) {
+            // This calls the method on our actual PaymentInterface
+            payment_line.payment_method.payment_terminal.send_payment_request(line.cid);
         }
     },
-    send_payment_request(line) {
-        if (line && line.payment_method && line.payment_method.payment_terminal) {
-            const iface = this.pos.payment_interfaces[line.payment_method.payment_terminal];
-            if (iface && iface.send_payment_request) {
-                iface.send_payment_request(line.cid);
-            }
+
+    /**
+     * This method handles the 'Force Confirm' action.
+     * @param {Object} line The payment line object passed from the template.
+     */
+    async send_force_done(line) {
+        const payment_line = this.currentOrder.get_paymentline(line.cid);
+        if (payment_line) {
+            // This calls the method on our actual PaymentInterface
+            payment_line.payment_method.payment_terminal.force_done(line.cid);
         }
     },
-    render(superRender) {
-        const vnode = superRender();
-        if (vnode && vnode.children) {
-            for (const child of vnode.children) {
-                if (child && child.type && child.type.name === 'PaymentScreenPaymentLines') {
-                    child.props = {
-                        ...child.props,
-                        send_force_done: this.send_force_done,
-                        send_payment_request: this.send_payment_request,
-                    };
-                }
-            }
-        }
-        return vnode;
-    },
+    
+    
 });
