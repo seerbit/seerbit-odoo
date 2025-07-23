8de0e9ee97d4aca9f70c7c97309eded3a4bc3819
kanebi <emmakanebi@gmail.com>
2025-07-23 22:18:28 +0100

    paymentine fix9010

--- pos_seerbit/static/src/js/PaymentScreen.js
+++ pos_seerbit/static/src/js/PaymentScreen.js
@@ -2,20 +2,42 @@
 
 import { PaymentScreen } from '@point_of_sale/app/screens/payment_screen/payment_screen';
 import { patch } from '@web/core/utils/patch';
-import { onMounted } from '@odoo/owl';
 
 patch(PaymentScreen.prototype, {
-    setup() {
-        super.setup();
-        onMounted(() => {
-            const pendingPaymentLine = this.currentOrder.paymentlines.find(
-                paymentLine => paymentLine.payment_method.use_payment_terminal === 'seerbit' &&
-                    (!paymentLine.is_done() && paymentLine.get_payment_status() !== 'pending')
-            );
-            if (pendingPaymentLine) {
-                const paymentTerminal = pendingPaymentLine.payment_method.payment_terminal;
-                pendingPaymentLine.set_payment_status('waitingSeerbit');
+    setup(superSetup) {
+        superSetup();
+        this.send_force_done = this.send_force_done.bind(this);
+        this.send_payment_request = this.send_payment_request.bind(this);
+    },
+    send_force_done(line) {
+        if (line && line.payment_method && line.payment_method.payment_terminal) {
+            const iface = this.pos.payment_interfaces[line.payment_method.payment_terminal];
+            if (iface && iface.send_force_done) {
+                iface.send_force_done(line);
             }
-        });
+        }
+    },
+    send_payment_request(line) {
+        if (line && line.payment_method && line.payment_method.payment_terminal) {
+            const iface = this.pos.payment_interfaces[line.payment_method.payment_terminal];
+            if (iface && iface.send_payment_request) {
+                iface.send_payment_request(line.cid);
+            }
+        }
+    },
+    render(superRender) {
+        const vnode = superRender();
+        if (vnode && vnode.children) {
+            for (const child of vnode.children) {
+                if (child && child.type && child.type.name === 'PaymentScreenPaymentLines') {
+                    child.props = {
+                        ...child.props,
+                        send_force_done: this.send_force_done,
+                        send_payment_request: this.send_payment_request,
+                    };
+                }
+            }
+        }
+        return vnode;
     },
 });
