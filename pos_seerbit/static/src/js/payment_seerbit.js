709a1785236a3c9401b610120d835b7b453a29a7
kanebi <emmakanebi@gmail.com>
2025-07-24 00:10:37 +0100

    stable 17.0

--- pos_seerbit/static/src/js/payment_seerbit.js
+++ pos_seerbit/static/src/js/payment_seerbit.js
@@ -157,6 +157,7 @@ export default class SeerbitPayment extends PaymentInterface {
     async send_payment_cancel(order, cid) {
         this.seerbit_was_cancelled = true;
         clearTimeout(this.seerbit_polling);
+        return Promise.resolve();
     }
 
     send_force_done(line) {
