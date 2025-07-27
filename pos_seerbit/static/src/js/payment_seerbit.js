adb9d67d86b0e0090f0d95eba555cc6470fa73e3
kanebi <emmakanebi@gmail.com>
2025-07-27 20:37:24 +0100

    Version 18.0 Force Done Fix2

--- pos_seerbit/static/src/js/payment_seerbit.js
+++ pos_seerbit/static/src/js/payment_seerbit.js
@@ -174,12 +174,13 @@ export default class SeerbitPayment extends PaymentInterface {
 
     async send_force_done(line) {
         if (line && line.payment_method_id && line.payment_method_id.use_payment_terminal === 'seerbit') {
-            console.log('line force done', line);
             line.set_payment_status('done');
+
             await this.env.services.dialog.add(AlertDialog, {
                 title: 'Seerbit Payment',
                 body: 'Payment forcibly confirmed as done.',
             });
+            this.render();  
         }
     }
 
