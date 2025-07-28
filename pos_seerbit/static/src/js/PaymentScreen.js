e3976e8b330059fcaa1608cbd18077792f30b036
kanebi <emmakanebi@gmail.com>
2025-07-28 07:47:51 +0100

    fix on cancelled0

--- pos_seerbit/static/src/js/PaymentScreen.js
+++ pos_seerbit/static/src/js/PaymentScreen.js
@@ -25,5 +25,42 @@ patch(PaymentScreen.prototype, {
             line
         );
     },
+    paymentMethodImage(id) {
+        if (this.paymentMethod.use_payment_terminal === "seerbit") {
+            return "/pos_seerbit/static/description/icon.png";
+        }
+        if (this.paymentMethod.image) {
+            return `/web/image/pos.payment.method/${id}/image`;
+        } else if (this.paymentMethod.type === "cash") {
+            return "/point_of_sale/static/src/img/money.png";
+        } else if (this.paymentMethod.type === "pay_later") {
+            return "/point_of_sale/static/src/img/pay-later.png";
+        }  else {
+            return "/point_of_sale/static/src/img/card-bank.png";
+        }
+    },
+    deletePaymentLine(uuid) {
+        console.log('deleting seerbit payment', uuid);
+        const line = this.paymentLines.find( (line) => line.uuid === uuid);
+        if (line.payment_method_id.payment_method_type === "qr_code") {
+            this.currentOrder.remove_paymentline(line);
+            this.numberBuffer.reset();
+            return;
+        }
+        if (["waiting", "waitingCard", "timeout"].includes(line.get_payment_status()) && line.payment_method_id.payment_terminal) {
+            console.log('cancelling seerbit payment', uuid);
+            line.set_payment_status("waitingCancel");
+            line.payment_method_id.payment_terminal.send_payment_cancel(this.currentOrder, uuid).then( () => {
+                this.currentOrder.remove_paymentline(line);
+                this.numberBuffer.reset();
+            }
+            );
+        } else if (line.get_payment_status() !== "waitingCancel") {
+            console.log('removing seerbit payment', uuid);
+            this.currentOrder.remove_paymentline(line);
+            this.numberBuffer.reset();
+        }
+    }
+
     
 });
