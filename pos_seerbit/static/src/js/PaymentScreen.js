0dc32966b934f22b42f0dac72ee915df960f6353
kanebi <emmakanebi@gmail.com>
2025-07-28 06:56:23 +0100

    fix on cancelled2

--- pos_seerbit/static/src/js/PaymentScreen.js
+++ pos_seerbit/static/src/js/PaymentScreen.js
@@ -24,5 +24,19 @@ patch(PaymentScreen.prototype, {
          await payment_terminal.send_force_done(
             line
         );
+    },
+    async sendPaymentCancel(line) {
+        const payment_terminal = line.payment_method_id.payment_terminal;
+        line.set_payment_status("waitingCancel");
+        const isCancelSuccessful = await payment_terminal.send_payment_cancel(
+            this.currentOrder,
+            line.uuid
+        );
+        if (isCancelSuccessful) {
+            line.set_payment_status("retry");
+            this.pos.paymentTerminalInProgress = false;
+        } else {
+            line.set_payment_status("waitingCard");
+        }
     }
 });
