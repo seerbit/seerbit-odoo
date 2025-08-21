9bef0873da2eb5b015dc5aac47eefd006b5ddb26
kanebi <emmakanebi@gmail.com>
2025-08-21 22:26:56 +0100

    order_id standard uuid

--- pos_seerbit/static/src/js/payment_seerbit.js
+++ pos_seerbit/static/src/js/payment_seerbit.js
@@ -67,12 +67,12 @@ export default class SeerbitPayment extends PaymentInterface {
             const metadata = JSON.stringify({
                 'created_by': 'odoo_pos_seerbit',
                 'created_time': now.toISOString(),
-                'order_id': order?.id || order.uid,
+                'order_id': paymentLine.order_id?.uuid || order?.id || order.uid,
                 'pos_config_id': this.pos.config?.id,
                 'user_id': this.pos.user?.id
             });
         return {
-                "id": order?.id || order.uid?.toString(),
+                "id": paymentLine.order_id?.uuid || order?.id || order.uid?.toString(),
                 "posid": paymentMethod?.seerbit_terminal_id || "",
                 "merchantid": "",
                 "metadata": metadata,
@@ -183,7 +183,6 @@ export default class SeerbitPayment extends PaymentInterface {
 
             // Mark payment as done and finalize the payment line
             paymentLine.set_payment_status('done');
-            paymentLine.set_paid();
             
             // Update the order state
             const order = this.pos.get_order();
@@ -193,8 +192,12 @@ export default class SeerbitPayment extends PaymentInterface {
                 
                 // Finalize the order if all payments are complete
                 if (order.is_paid) {
-                    order.finalize();
-                    console.log('finalized')
+                    try{
+                        order?.finalize();
+                        console.log('finalized')
+                    }catch(error){
+                        console.log('error finalizing order', error)
+                    }
                     
                 }
                 console.log('setting done again')
@@ -271,7 +274,7 @@ export default class SeerbitPayment extends PaymentInterface {
     async send_force_done(line) {
         if (line && line.payment_method_id && line.payment_method_id.use_payment_terminal === 'seerbit') {
             line.set_payment_status('done');
-            line.set_receipt_info('Transaction ID: ' + (line.pos_order_id?.uuid || line.pos_order_id?.id)?.toString());
+            line.set_receipt_info('Transaction ID: ' + (line.pos_order_id?.uuid || line.order_id.uuid)?.toString());
             clearTimeout(this.seerbit_polling);
             
             // Clean up localStorage to prevent "electronic payment in progress" error
